d7077bb1795cdb3088d2210fd7b2c157d2e07217
P.R. Neider-Ball <phillrballn@icloud.com>
2025-10-08 15:23:58 -0500

    Initial commit

---
+++ se_project_express/routes/users.js
@@ -0,0 +1,8 @@
+const router = require("express").Router();
+const { getCurrentUser, updateUserProfile } = require("../controllers/users");
+
+router.get("/me", getCurrentUser);
+
+router.patch("/me", updateUserProfile);
+
+module.exports = router;
